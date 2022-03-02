ea55087cee87664ebbaaa0df58d6911c7644da42
hoodiesquad <hoodiesqd@gmail.com>
2022-03-03 00:58:05 +0600

    Init commit

---
+++ src/main/java/site/hoodiesquad/HoodiesquadApplication.java
@@ -0,0 +1,13 @@
+package site.hoodiesquad;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HoodiesquadApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HoodiesquadApplication.class, args);
+    }
+
+}
---
+++ src/test/java/site/hoodiesquad/HoodiesquadApplicationTests.java
@@ -0,0 +1,13 @@
+package site.hoodiesquad;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HoodiesquadApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
