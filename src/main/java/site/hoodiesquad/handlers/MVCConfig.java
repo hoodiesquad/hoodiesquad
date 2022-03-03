e821ae913ce19fb1142ed7e1019ca4fe255e7b39
hoodiesquad <hoodiesqd@gmail.com>
2022-03-03 21:19:30 +0600

    Configure MVCConfig.

---
+++ src/main/java/site/hoodiesquad/handlers/MVCConfig.java
@@ -0,0 +1,17 @@
+package site.hoodiesquad.handlers;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+@EnableWebMvc
+public class MVCConfig  implements WebMvcConfigurer {
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry.addResourceHandler("/css/**").addResourceLocations("classpath:/static/css/");
+        registry.addResourceHandler("/js/**").addResourceLocations("classpath:/static/js/");
+        registry.addResourceHandler("/img/**").addResourceLocations("classpath:/static/img/");
+    }
+}
